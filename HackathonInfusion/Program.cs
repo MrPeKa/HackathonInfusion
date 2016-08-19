e494b4b17c335125a75a01682047c7ad95a7d5fd
Paweł Kunicki <pekunicki@Hotmail.com>
2016-08-19 16:14:24 +0200

    Prepared boilerplate for Project, Added Json.Net and WebApi.HttpClient

---
+++ HackathonInfusion/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace HackathonInfusion
+{
+
+    public class Program
+    {
+        public static void Main<T>()
+        {
+            RunAsync<T>().Wait();
+        }
+
+        public static async Task RunAsync<T>()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:9000/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                // HTTP GET
+                HttpResponseMessage response = await client.GetAsync("api/products/1");
+                if (response.IsSuccessStatusCode)
+                {
+                    T result = await response.Content.ReadAsAsync<T>();
+                }
+
+                // HTTP POST
+                var gizmo = new object();
+                response = await client.PostAsJsonAsync("api/products", gizmo);
+                if (response.IsSuccessStatusCode)
+                {
+                    Uri gizmoUrl = response.Headers.Location;
+
+                    // HTTP PUT
+                    response = await client.PutAsJsonAsync(gizmoUrl, gizmo);
+
+                    // HTTP DELETE
+                    response = await client.DeleteAsync(gizmoUrl);
+                }
+            }
+        }
+    }
+}
