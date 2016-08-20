e3efffb5fdc1d753192aecf935d0585228a09bfc
blisek <bartlomiej.lisek@hotmail.com>
2016-08-20 10:23:38 +0200

    Interfejs

---
+++ HackathonInfusion/PositionInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackathonInfusion
+{
+    public struct PositionInfo
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string Details { get; set; }
+        public bool Failure { get; set; }
+    }
+}
---
+++ HackathonInfusion/Solver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackathonInfusion
+{
+    public class Solver
+    {
+        private IConnection _connection;
+
+        public Solver(IConnection conn)
+        {
+            if (conn == null)
+                throw new ArgumentNullException("Connection null");
+
+            _connection = conn;
+        }
+
+
+    }
+}
